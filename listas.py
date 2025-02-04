1f1479eafe9dc9d98b399ad46ee601c2a5409340
adrianquintero09 <adrianquinteropinzon@gmail.com>
2025-02-04 08:55:22 -0500

    Mi segundo comentario

--- listas.py
+++ listas.py
@@ -67,17 +67,82 @@ apellidos = [
     ["Bastos", "Garcia"],
     ["Diaz", "Contreras"]
 ]
-
-print("Nombres:", nombres)
-print("Apellidos:", apellidos)
-
-booleanito= True
-while (booleanito==True):
-    print("\n//////Bienvenido al programa de lista de estudiantes//////")
-    print("//////======Que te gustaría hacer?======//////")
-    print("1. Cambiar nombre y apellido")
-    print("")
-    break
-opc = int(input("Ingresa una Opcion: "))
-if opc == 1:
-    print("")
+bo=True
+while bo==True:
+    print("////////////////////////////////////////////////////////")
+    print("///Bienvenido al programa de lista de estudiantes///////")
+    print("////////////////////////////////////////////////////////")
+    print("Que te gustaría hacer?")
+    print("1.Agregar estudiante")
+    print("2. Ver estudiantes")
+    print("3.Editar estudiante")
+    print("4.Eliminar estudiante")
+    print("5.Salir del programa")
+    opc=int(input(":"))
+    c1=0
+    c=0
+    if opc==1:
+        print("ingrese el primer nombre del estudiante")
+        n1=input(":")
+        print("ingrese el segundo nombre (si lo tiene)")
+        n2=input(":")
+        print("Ingrese el primer apellido")
+        ap1=input(":")
+        print("ingrese el segundo apellido")
+        ap2=input(":")
+        nombres.append([n1,n2])
+        apellidos.append([ap1,ap2])
+    elif opc==2:
+        for i in range(len(nombres)):
+            ##nombres = [["Adrián"]] ---- apellidos = [["Quintero", "Pinzón"]] --- Adrían Quintero Pinzón
+            print("estudiante#",i+1," "," ".join(nombres[c1])," ".join(apellidos[c]))
+            c1+=1
+            c+=1
+    elif opc==3:
+        for i in range(len(nombres)):
+            ##nombres = [["Adrián"]] ---- apellidos = [["Quintero", "Pinzón"]] --- Adrían Quintero Pinzón
+            print("estudiante#",i+1," "," ".join(nombres[c1])," ".join(apellidos[c]))
+            c1+=1
+            c+=1
+        edt=int(input("Ingrese el estudiante que quiere editar (el numero de la lista)"))
+        tamnombre=len(nombres[edt-1])
+        tamapellido=len(apellidos[edt-1])
+        if tamnombre==2:
+            print("1. Primero nombre")
+            print("2. Segundo nombre")
+            o=int(input(": "))
+            r=input("Ingrese el nombre corregido: ")
+            if o==1:
+                nombres[edt-1][0]=(r)
+            else:
+                nombres[edt-1][1]=(r)
+        else:
+            print("Ingrese el nombre corregido")
+            r=input(": ")
+            nombres[edt-1][0]=(r)
+        if tamapellido==2:
+            print("1. Primer apellido")
+            print("2. Segundo apellido")
+            t=int(input(": "))
+            if t==1:
+                ap=input("Digite el primer apellido corregido ")
+                apellidos[edt-1][0]=(ap)
+            else:
+                ap=input("Digite el segundo apellido")
+                apellidos[edt-1][1]=(ap)
+        else:
+            apr=input("Digite el apellido corregido")
+            apellidos[edt-1][0]=(apr)
+    elif opc==4:
+        for i in range(len(nombres)):
+            ##nombres = [["Adrián"]] ---- apellidos = [["Quintero", "Pinzón"]] --- Adrían Quintero Pinzón
+            print("estudiante#",i+1," "," ".join(nombres[c1])," ".join(apellidos[c]))
+            c1+=1
+            c+=1
+        el=int(input("ingrese el estudiante que quiere eliminar (el numero de la lista)"))
+        nombres.pop(el-1)
+        apellidos.pop(el-1)
+    elif opc==5:
+        break
+    else:
+        print("La opcion que ingreso es incorrecta")
